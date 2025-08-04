21ef74f7584695a5949a8294c2ad122608916b61
Millham1 <145725492+Millham1@users.noreply.github.com>
2025-08-04 00:20:20 -0400

    Create Sp500LatestNewsApplication.java

---
+++ src/main/java/com/example/sp500latestnews/Sp500LatestNewsApplication.java
@@ -0,0 +1,11 @@
+package com.example.sp500latestnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp500LatestNewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Sp500LatestNewsApplication.class, args);
+    }
+}
